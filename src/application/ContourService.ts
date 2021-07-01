0b5117a9dde75e50964cf7e200627c5664099c5d
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-07-01 17:40:26 -0300

    Implementation of controls for input data using turf.js

--- src/application/ContourService.ts
+++ src/application/ContourService.ts
@@ -1,3 +1,6 @@
+import area from "@turf/area";
+import booleanContains from "@turf/boolean-contains";
+import { Feature, polygon } from "@turf/helpers";
 import { inject, injectable } from "tsyringe";
 
 import Equidistance from "../domain/Equidistance";
@@ -18,15 +21,16 @@ export default class ContourService {
       `[
         {
           "name": "Capa",
-          "type": "select",
+          "element": "select",
           "references": "drawedLayers",
           "allowedTypes": ["rectangle"],
           "points": ["ne", "sw"]
         },
         {
           "name": "Equidistancia",
-          "type": "integer",
-          "min": 100,
+          "element": "input",
+          "type": "number",
+          "min": 10,
           "max": 10000
         }
       ]`
@@ -41,6 +45,14 @@ export default class ContourService {
     equidistance: Equidistance,
     wpsEndpoint: wpsEndpoint
   ): Promise<JSON> {
+    this.ensureInputDataIsInTolerance(
+      longitudeLower,
+      latitudeLower,
+      longitudeUpper,
+      latitudeUpper,
+      equidistance
+    );
+
     const inputXml = `<?xml version="1.0" encoding="UTF-8"?><wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
   <ows:Identifier>gs:Contour</ows:Identifier>
   <wps:DataInputs>
@@ -97,5 +109,63 @@ export default class ContourService {
       `${wpsEndpoint.value}&request=Execute&identifier=gs:Contour`,
       inputXml
     );
+  }
+
+  pointsToPolygon(
+    longitudeLower: Longitude, //x2
+    latitudeLower: Latitude, //y2
+    longitudeUpper: Longitude, //x1
+    latitudeUpper: Latitude //y1
+  ): Feature {
+    const x = polygon([
+      [
+        [longitudeUpper.value, latitudeUpper.value], //x1y1
+        [longitudeUpper.value, latitudeLower.value], //x1y2
+        [longitudeLower.value, latitudeLower.value], //x2y2
+        [longitudeLower.value, latitudeUpper.value], //x2y1
+        [longitudeUpper.value, latitudeUpper.value], //x1y1
+      ],
+    ]);
+
+    return x;
+  }
+
+  ensureInputDataIsInTolerance(
+    longitudeLower: Longitude,
+    latitudeLower: Latitude,
+    longitudeUpper: Longitude,
+    latitudeUpper: Latitude,
+    equidistance: Equidistance
+  ): void {
+    const inputPolygon = this.pointsToPolygon(
+      longitudeLower,
+      latitudeLower,
+      longitudeUpper,
+      latitudeUpper
+    );
+
+    if (area(inputPolygon) > 100000000) {
+      throw RangeError("The area requested must be less than 100km2");
+    }
+
+    const elevatedSurfaces = polygon([
+      [
+        [-73.904443327547412, -51.888469736942632],
+        [-70.88203831519283, -20.86255705909867],
+        [-62.863412772211284, -21.417692673612777],
+        [-64.343774410915572, -28.696137397242179],
+        [-62.678367567373243, -28.881182602080212],
+        [-66.502635134025979, -52.690332291240786],
+        [-73.904443327547412, -51.888469736942632],
+      ],
+    ]);
+
+    const interseccion = booleanContains(elevatedSurfaces, inputPolygon);
+
+    if (interseccion == true && equidistance.value < 100) {
+      throw RangeError("Equidistance must be grather than 100");
+    } else if (equidistance.value < 10) {
+      throw RangeError("Equidistance must be grather than 10");
+    }
   }
 }
--- tests/ContourHandler.spec.ts
+++ tests/ContourHandler.spec.ts
@@ -19,15 +19,16 @@ test("Get WPS Contour form", () => {
     `[
       {
         "name": "Capa",
-        "type": "select",
+        "element": "select",
         "references": "drawedLayers",
         "allowedTypes": ["rectangle"],
         "points": ["ne", "sw"]
       },
       {
         "name": "Equidistancia",
-        "type": "integer",
-        "min": 100,
+        "element": "input",
+        "type": "number",
+        "min": 10,
         "max": 10000
       }
     ]`
@@ -52,7 +53,18 @@ test("Execute succesful WPS Contour", async () => {
   ).toEqual(postmanTest.getResponseTest());
 });
 
-test("Execute WPS Contour and get Exception", async () => {
+test("Execute succesful WPS Contour when request area touch higher and lower surface", async () => {
+  const postmanTest = new PostmanTest();
+  const contourHandler = new ContourHandler(
+    "http://127.0.0.1:8080/geoserver/ows?service=WPS&version=1.0.0",
+    container.resolve(ContourService)
+  );
+  expect(
+    await contourHandler.execute(-63.53331, -34.17846, -63.52048, -34.16922, 10)
+  ).toEqual(postmanTest.getResponseTest());
+});
+
+test("Execute WPS Contour and get Coordinates Exception", async () => {
   const contourHandler = new ContourHandler(
     "http://127.0.0.1:8080/geoserver/ows?service=WPS&version=1.0.0",
     container.resolve(ContourService)
@@ -63,4 +75,85 @@ test("Execute WPS Contour and get Exception", async () => {
   expect(
     async () => await contourHandler.execute(1, 1, 1, 1, 1)
   ).rejects.toThrow("The latitud must be between -74 and -52");
+});
+
+test("Execute WPS Contour and get Area Requested Exception", async () => {
+  const contourHandler = new ContourHandler(
+    "http://127.0.0.1:8080/geoserver/ows?service=WPS&version=1.0.0",
+    container.resolve(ContourService)
+  );
+  expect(
+    async () =>
+      await contourHandler.execute(
+        -63.68053,
+        -34.02598,
+        -63.22427,
+        -33.82642,
+        100
+      )
+  ).rejects.toThrow(RangeError);
+  expect(
+    async () =>
+      await contourHandler.execute(
+        -63.68053,
+        -34.02598,
+        -63.22427,
+        -33.82642,
+        100
+      )
+  ).rejects.toThrow("The area requested must be less than 100km2");
+});
+
+test("Execute WPS Contour and get Equidistance Range Exception in Higher Surface", async () => {
+  const contourHandler = new ContourHandler(
+    "http://127.0.0.1:8080/geoserver/ows?service=WPS&version=1.0.0",
+    container.resolve(ContourService)
+  );
+  expect(
+    async () =>
+      await contourHandler.execute(
+        -69.28589006035278,
+        -28.47228576296026,
+        -68.81897111654268,
+        -28.16279458245836,
+        10
+      )
+  ).rejects.toThrow(RangeError);
+  expect(
+    async () =>
+      await contourHandler.execute(
+        -69.28589006035278,
+        -28.47228576296026,
+        -68.81897111654268,
+        -28.16279458245836,
+        10
+      )
+  ).rejects.toThrow("Equidistance must be grather than 100");
+});
+
+test("Execute WPS Contour and get Equidistance Range Exception in Lower Surface", async () => {
+  const contourHandler = new ContourHandler(
+    "http://127.0.0.1:8080/geoserver/ows?service=WPS&version=1.0.0",
+    container.resolve(ContourService)
+  );
+  expect(
+    async () =>
+      await contourHandler.execute(
+        -60.37306160555501,
+        -36.299503918476745,
+        -58.792194242187236,
+        -35.76065272730704,
+        9
+      )
+  ).rejects.toThrow(RangeError);
+  expect(
+    async () =>
+      await contourHandler.execute(
+        -69.28589006035278,
+        -28.47228576296026,
+        -68.81897111654268,
+        -28.16279458245836,
+        9
+      )
+  ).rejects.toThrow("Equidistance must be grather than 10");
 });
