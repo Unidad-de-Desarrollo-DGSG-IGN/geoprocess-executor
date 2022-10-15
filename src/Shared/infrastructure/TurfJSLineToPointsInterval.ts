c2169bb620a62470e39c834e25b41c3d4c1c9f2f
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2022-10-15 15:08:01 -0300

    Fix not over line points

--- src/Shared/infrastructure/TurfJSLineToPointsInterval.ts
+++ src/Shared/infrastructure/TurfJSLineToPointsInterval.ts
@@ -1,11 +1,16 @@
 import along from "@turf/along";
-import { Feature, feature, Properties } from "@turf/helpers";
+import buffer from "@turf/buffer";
+import { Feature, feature, FeatureCollection, Properties } from "@turf/helpers";
+import { getCoords } from "@turf/invariant";
 import length from "@turf/length";
+import lineIntersect from "@turf/line-intersect";
+import pointToLineDistance from "@turf/point-to-line-distance";
 import { injectable } from "tsyringe";
 
 import Line from "../domain/Line";
 import LineToPointsInterval from "../domain/LineToPointsInterval";
 import MultiPointInLine from "../domain/MultiPointInLine";
+import Point from "../domain/Point";
 import PointInLine from "../domain/PointInLine";
 
 @injectable()
@@ -13,37 +18,19 @@ export default class TurfJSLineToPointsInterval
   implements LineToPointsInterval
 {
   private static readonly INTERMEDIATE_POINTS_IN_LINE: number = 100;
+  private static readonly BUFFER_RADIOUS_CONSTANT: number = 1.1;
+  private static readonly BUFFER_APPROXIMATION_CONSTANT: number = 0.01;
 
   execute(line: Line): MultiPointInLine {
-    const geometry = JSON.parse(`
-      {
-        "type": "LineString",
-        "coordinates": [${line.toString()}]
-      }`);
-    const lineFeature = feature(geometry);
+    return this.discatrizeLine(line);
+  }
 
-    const lineLenght: number = length(lineFeature, { units: "meters" });
-    const distanceBetweenPoints: number =
-      this.calculateDistanceBetweenPoints(lineLenght);
-    let point: any;
-    const points: MultiPointInLine = new MultiPointInLine();
-    if (distanceBetweenPoints > 0) {
-      this.getIntermediatePoints(
-        lineFeature,
-        lineLenght,
-        distanceBetweenPoints
-      ).forEach((pointInLine) => {
-        points.add(pointInLine);
-      });
-    } else {
-      points.add(this.getFirstNode(line));
-    }
+  private calculateLineLength(line: Feature): number {
+    return length(line, { units: "meters" });
+  }
 
-    this.getAllNodesExceptFirst(line).forEach((pointInLine) => {
-      points.add(pointInLine);
-    });
-
-    return points;
+  private getLineVertices(line: Feature<any, Properties>): any[] {
+    return getCoords(line);
   }
 
   private calculateDistanceBetweenPoints(lineLenght: number): number {
@@ -52,44 +39,110 @@ export default class TurfJSLineToPointsInterval
     );
   }
 
-  private getIntermediatePoints(
-    lineFeature: Feature<any, Properties>,
-    lineLenght: number,
-    distanceBetweenPoints: number
-  ): PointInLine[] {
-    let point: any;
-    const pointsInLine: PointInLine[] = [];
+  private discatrizeLine(line: Line): MultiPointInLine {
+    let calculatedPoint: any;
+    const pointsInLine: MultiPointInLine = new MultiPointInLine();
+    const lineFeature = this.convertLineDomainToTurfFeature(line);
+    const lineLenght: number = this.calculateLineLength(lineFeature);
+    const distanceBetweenPoints: number =
+      this.calculateDistanceBetweenPoints(lineLenght);
 
-    for (let step = 0; step < lineLenght; step += distanceBetweenPoints) {
-      point = along(lineFeature, step, { units: "meters" });
-      pointsInLine.push(
-        PointInLine.create(
-          `${point.geometry.coordinates[0]} ${point.geometry.coordinates[1]}`,
-          step
-        )
-      );
+    if (distanceBetweenPoints > 0) {
+      for (
+        let distance = distanceBetweenPoints;
+        distance < lineLenght;
+        distance += distanceBetweenPoints
+      ) {
+        calculatedPoint = this.calculateNearPointAtDistance(
+          lineFeature,
+          distance
+        );
+        pointsInLine.add(
+          PointInLine.create(
+            `${calculatedPoint.geometry.coordinates[0]} ${calculatedPoint.geometry.coordinates[1]}`,
+            distance
+          )
+        );
+      }
     }
+
+    this.getAllLineNodes(line).forEach((pointInLine) => {
+      pointsInLine.add(pointInLine);
+    });
 
     return pointsInLine;
   }
 
-  private getFirstNode(line: Line): PointInLine {
+  private convertLineDomainToTurfFeature(line: Line): Feature {
     const geometry = JSON.parse(`
-    {
-      "type": "LineString",
-      "coordinates": [${line.subLine(0)}]
-    }`);
-    const lineFeature = feature(geometry);
-    return PointInLine.create(
-      `${line.value[0].longitude.value} ${line.value[0].latitude.value}`,
-      length(lineFeature, { units: "meters" })
-    );
+      {
+        "type": "LineString",
+        "coordinates": [${line.toString()}]
+      }`);
+    return feature(geometry);
   }
 
-  private getAllNodesExceptFirst(line: Line): PointInLine[] {
+  private calculateNearPointAtDistance(
+    line: Feature<any, Properties>,
+    distance: number
+  ): Feature {
+    const nearPoint = along(line, distance, { units: "meters" });
+    const nearPointToLineDistance = pointToLineDistance(nearPoint, line, {
+      units: "meters",
+    });
+    let bufferRadius =
+      nearPointToLineDistance *
+      TurfJSLineToPointsInterval.BUFFER_RADIOUS_CONSTANT;
+    let interesctionPoints = this.NearPointToLineIntersect(
+      nearPoint,
+      line,
+      bufferRadius,
+      distance
+    );
+    while (
+      interesctionPoints == null &&
+      bufferRadius < TurfJSLineToPointsInterval.BUFFER_RADIOUS_CONSTANT * 3
+    ) {
+      bufferRadius =
+        bufferRadius * TurfJSLineToPointsInterval.BUFFER_APPROXIMATION_CONSTANT;
+      interesctionPoints = this.NearPointToLineIntersect(
+        nearPoint,
+        line,
+        bufferRadius,
+        distance
+      );
+    }
+
+    if (interesctionPoints == null) {
+      console.warn(interesctionPoints);
+    }
+
+    return interesctionPoints!.features[0];
+  }
+
+  private NearPointToLineIntersect(
+    nearPoint: Feature,
+    line: Feature<any, Properties>,
+    bufferRadius: number,
+    distance: number
+  ): FeatureCollection<any, Properties> | null {
+    if (bufferRadius == 0) {
+      bufferRadius = 0.0001;
+    }
+    const nearPointBuffered = buffer(nearPoint, bufferRadius, {
+      units: "meters",
+    });
+    const interesctionPoints = lineIntersect(line, nearPointBuffered);
+    if (interesctionPoints.features[0] == undefined) {
+      return null;
+    }
+    return interesctionPoints;
+  }
+
+  private getAllLineNodes(line: Line): PointInLine[] {
     const pointsInLine: PointInLine[] = [];
 
-    for (let pointIndex = 1; pointIndex < line.value.length; pointIndex++) {
+    for (let pointIndex = 0; pointIndex < line.value.length; pointIndex++) {
       const geometry = JSON.parse(`
       {
         "type": "LineString",
